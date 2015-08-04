63af922ad271d2df32d2f0bd4a5366205a4a933c
Mike Lewis <mike@updroid.com>
2015-08-03 18:08:37 -0700

    bin: added explorer import to main

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'dart:isolate';
 import 'package:upcom-api/tab_backend.dart';
+import '../lib/explorer.dart';
 
 void main(List args, SendPort interfacesSendPort) {
   Panel.main(interfacesSendPort, args, (port, args) => new CmdrExplorer(port, args));
